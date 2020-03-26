ec19e298294b2e9d3ec85bbe3d3e75b130fd143a
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-03-27 02:05:16 +1100

    Auto-updater

--- discordbot/main.py
+++ discordbot/main.py
@@ -28,6 +28,7 @@ class Bot(discord.ext.commands.Bot):
         super().__init__(command_prefix='~')
         super().load_extension('roles.colour')
         super().load_extension('discordbot.owner')
+        super().load_extension('discordbot.updater')
         super().load_extension('notices_channel')
         super().load_extension('spoilerchan.spoilers')
 
