7c6618480522b0666cc55807774a7d2a019e4514
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-05-07 18:31:56 +1000

    Fix /setcolour

--- roles/colour.py
+++ roles/colour.py
@@ -21,29 +21,31 @@ class Roles(Extension):
     @slash_option("colour", "The colour to set", OptionType.STRING, required=True)
     async def setcolour(self, ctx: SlashContext, colour: Color) -> None:
         await self.remove_colour(ctx.author)
-        role = await self.get_or_create_colour_role(colour, ctx.guild)
-        await ctx.author.add_roles(role)
+        try:
+            role = await self.get_or_create_colour_role(colour, ctx.guild)
+        except ValueError as e:
+            await ctx.send(str(e))
+            return
+        await ctx.author.add_role(role)
         await ctx.send("Done!")
 
-    # @setcolour.error
-    # async def colour_error(self, ctx, error):
-    #     if isinstance(error, commands.MissingRequiredArgument):
-    #         await ctx.send('You need to give me a color')
-    #         return
-
-    #     if isinstance(error, commands.UserInputError):
-    #         await ctx.send("That's not a colour I recognise")
-    #         return
-    #     await ctx.send("An unknown error occured")
+    @setcolour.error
+    async def colour_error(self, error, ctx, colour):
+        if isinstance(error, KeyError):
+            await ctx.send("That's not a colour I recognise.  Hex codes are `#RRGGBB`.")
+            return
+        await ctx.send("An unknown error occured")
 
     async def remove_colour(self, member: Member) -> None:
         for role in member.roles:
             if role.name.startswith("Colour: "):
-                await member.remove_roles(role)
+                await member.remove_role(role)
                 if not role.members:
                     await role.delete()
 
     async def get_or_create_colour_role(self, colour: Color, guild: Guild) -> Role:
+        if isinstance(colour, str):
+            colour = Color(colour)
         name = f"Colour: {str(colour)}"
         for role in guild.roles:
             if role.name == name:
